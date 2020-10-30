08bcd3bd95e67a727c75fa3b503e2f0ebce12baa
James Coleman <jcolem22@msudenver.edu>
2020-10-30 14:56:26 -0600

    Added some face detection code and cropped the input images based on the results.

--- facemask_detection/detection.py
+++ facemask_detection/detection.py
@@ -19,16 +19,57 @@
     #   
 
 # Imports
+import numpy
+import cv2 
+from matplotlib import pyplot
 
+# Helper methods
+def show(image, title):
+    """ Helper method to display a single image 
+    with pyplot """
+    pyplot.figure()
+    pyplot.title(title)
+    pyplot.imshow(image)
+    pyplot.show()
+
+def isolate_face(image, cascade):
+	""" Uses cv's CascadeClassifier to detect a face and then
+	remove a border around the face."""
+	face_cascade = cv2.CascadeClassifier(cascade)
+	crop_wOut_mask = face_cascade.detectMultiScale(
+		image,
+		scaleFactor = 1.1,
+		minNeighbors = 5,
+		minSize = (30, 30),
+	)
+
+	for (x, y, w, h) in crop_wOut_mask:
+		cut_image = image[y - 40: y + h + 80, x: x + w, :]
+
+	return cut_image		
+	
 
 # Collect and sanitize input images
 ## get image
+wOut_mask = cv2.imread('pp1.png')
+with_mask = cv2.imread('pp2.png')
+
 ## change image's color format
+with_mask = cv2.cvtColor(with_mask, cv2.COLOR_BGR2RGB)
+wOut_mask = cv2.cvtColor(wOut_mask, cv2.COLOR_BGR2RGB)
 ## get image dimensions?
 
+show(wOut_mask, "Without mask")
+show(with_mask, "With mask")
 
 # Cut out head and neck region out of images
-## week 10 material
+## use Haar cascade provided by professor Feng
+cascade_file = "haarcascade_frontalface_default.xml"
+wOut_mask = isolate_face(wOut_mask, cascade_file)
+with_mask = isolate_face(with_mask, cascade_file)
+
+show(wOut_mask, "Without mask")
+show(with_mask, "With mask")
 
 
 # Detect the skin area of the new images
