f1cfe289060839ffe49f68e88552f13c41eb75e6
James Coleman <jcolem22@msudenver.edu>
2020-10-30 13:18:03 -0600

    Added input files and completed a comment outline

---
+++ facemask_detection/detection.py
@@ -0,0 +1,48 @@
+#===============================================================#
+#                                                               #
+#   James Coleman                                               #
+#   CS 3150                                                     #
+#   Project 1                                                   #
+#   October 29th                                                #
+#                                                               #
+#===============================================================#
+
+    #          >>>>>>>>>> Goals <<<<<<<<<<
+    #   
+    #   1. Cut out head and neck region out of images
+    #   2. Detect the skin area of the new images
+    #   3. Replace the mask region of the mask image with the 
+    #   	corresponding skin region of the other image
+	#	4. Repair any evidence of the image crossover
+	#	5. Enhance the image
+    #   
+    #   
+
+# Imports
+
+
+# Collect and sanitize input images
+## get image
+## change image's color format
+## get image dimensions?
+
+
+# Cut out head and neck region out of images
+## week 10 material
+
+
+# Detect the skin area of the new images
+## consult code from homework 3    
+
+
+# Replace the mask region of that image with the corresponding 
+# skin region of the other image
+	
+
+# Repair any evidence of the image crossover
+## do some smoothing perhaps	
+
+# Enhance the image
+## choose a couple techniques from homework 2
+
+
